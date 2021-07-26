106760a236c5e735379fa37cd4d80ada5f41408e
Gevork <64646814+socialcolor@users.noreply.github.com>
2021-07-26 22:23:33 +0300

    disabled sticks and clean sticks

--- source/js/index.js
+++ source/js/index.js
@@ -1,23 +1,58 @@
 'use strict';
 
-const sticks = document.querySelectorAll('.sticks__input');
+const sticksGroup = document.querySelectorAll('.sticks__group');
+const sticksLabel = document.querySelectorAll('.sticks__label');
+const sticksInput = document.querySelectorAll('.sticks__input');
 const stickWrapper = document.querySelector('.sticks__wrapper');
 const hideButton = document.querySelector('.buttons button');
 
-const onStickClick = (event) => {
-  const stickClick = event.target.classList.contains('sticks__stick');
-  if (stickClick) {
-    event.target.classList.toggle('sticks__stick--checked');
+const disabledSticks = (event, enable) => {
+  const eventGroup = event.target.parentElement;
+  if(enable) {
+    for(let input of sticksInput) {
+
+    }
+  } else {
+    for(let group of sticksGroup) {
+      if(group !== eventGroup) {
+        const sticksGroup = group.querySelectorAll('input');
+        for (let input of sticksGroup) {
+          input.disabled = true;
+        }
+      }
+    }
+  }
+}
+
+const enabledStick = () => {
+  const label = document.querySelectorAll('.sticks__label--checked');
+  if(label.length === 0) {
+    for (let input of sticksInput) {
+      input.disabled = false;
+    }
+  }
+}
+
+const onStickClick = function (event) {
+  event.stopPropagation();
+  const stickDisabled = event.target.querySelector('.sticks__input:not([disabled])')
+  if (stickDisabled) {
+    event.target.classList.toggle('sticks__label--checked');
+    disabledSticks(event);
+    enabledStick();
   }
 };
 
 const onHideButtonHide = () => {
-  sticks.forEach((element) => {
-    if(element.checked) {
-      element.parentElement.classList.add('sticks__stick--hide');
+  sticksInput.forEach((element) => {
+    if (element.checked) {
+      element.parentElement.remove();
     }
   })
+  enabledStick();
 };
 
-stickWrapper.addEventListener('click', onStickClick)
+for (let label of sticksLabel) {
+  label.addEventListener('click', onStickClick)
+}
 hideButton.addEventListener('click', onHideButtonHide)
